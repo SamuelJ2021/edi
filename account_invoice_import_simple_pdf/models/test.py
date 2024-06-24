ab5653c1bd324507c3dc6a702e8ba72805e8db09
SamuelJ2021 <77405836+SamuelJ2021@users.noreply.github.com>
2024-06-24 11:11:14 +0200

    Create test.py

---
+++ account_invoice_import_simple_pdf/models/test.py
@@ -0,0 +1,5 @@
+# Script qui permet d'importer et de créer la facture fournisseur dans Odoo à partir du fichier json généré par Google AI document
+
+# 1\ Récupérer le fichier json généré par Google AI document
+# 2\ Importer un fichier json
+# 3\ Créer la facture fournisseur
